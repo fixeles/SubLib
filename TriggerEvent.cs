c3defea624ed964ceca8a4a362a3a695d66cbf50
tka2704 <tka2704@gmail.com>
2022-10-25 15:02:51 +0300

    trigger_radius

--- TriggerEvent.cs
+++ TriggerEvent.cs
@@ -8,6 +8,22 @@ public class TriggerEvent : MonoBehaviour
     public event Action<Collider> OnTriggerStayEvent;
     public event Action<Collider> OnTriggerExitEvent;
 
+    private float _radius;
+
+    public float Radius
+    {
+        get
+        {
+            if (_radius == 0)
+            {
+                if (TryGetComponent(out SphereCollider sphereCollider))
+                    _radius = sphereCollider.radius;
+            }
+
+            return _radius;
+        }
+    }
+
 
     private void OnTriggerEnter(Collider other)
     {
