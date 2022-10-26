402bab48068ad158ffdc130ac25cff5b2b8a695f
tka2704 <tka2704@gmail.com>
2022-10-26 19:33:15 +0300

    run_state_rework

--- States/PlayerRunState.cs
+++ States/PlayerRunState.cs
@@ -11,13 +11,16 @@ public struct PlayerRunState : IState
     {
         const float defaultMoveSpeed = 5;
         Vector3 moveDirrection =
-            Player.Instance.Input.Direction * defaultMoveSpeed * Player.Instance.MoveSpeedMultiplier;
-        Player.Instance.Controller.SimpleMove(moveDirrection);
+            defaultMoveSpeed * Player.Instance.MoveSpeedMultiplier * Player.Instance.Input.Direction;
+        moveDirrection.y = Player.Instance.Rigidbody.velocity.y;
+        Player.Instance.Rigidbody.velocity = moveDirrection;
 
-        Vector3 velocity = new Vector3(Player.Instance.Controller.velocity.x, 0, Player.Instance.Controller.velocity.z);
+        Vector3 velocity = new Vector3(Player.Instance.Rigidbody.velocity.x, 0, Player.Instance.Rigidbody.velocity.z);
         if (velocity.magnitude > 0)
         {
             Player.Instance.transform.rotation = Quaternion.LookRotation(velocity.normalized);
+            // Quaternion.Lerp(Player.Instance.transform.rotation,
+            // Quaternion.LookRotation(velocity.normalized), Time.deltaTime * 20);
         }
 
         if (!Player.Instance.Input.IsActive()) Player.Instance.Statable.SetState(UnitState.Idle);
