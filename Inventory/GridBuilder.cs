b8325692c33a7a19d8a1ef574e09daaa1ab546b6
tka2704 <tka2704@gmail.com>
2022-10-30 18:02:22 +0300

    grid_axis

--- Inventory/GridBuilder.cs
+++ Inventory/GridBuilder.cs
@@ -5,12 +5,19 @@ namespace Game.Scripts.UtilsSubmodule.Inventory
 {
     public class GridBuilder : MonoBehaviour
     {
-        [field: SerializeField] public Vector3[] Grid = new Vector3[0];
+        [field: SerializeField] public Vector3[] Positions = new Vector3[0];
         [SerializeField] private Vector3 _spacing = Vector3.one;
         [SerializeField, Min(1)] private Vector2Int _size = Vector2Int.one;
         [SerializeField, Min(1)] private int _positionsCount = 1;
         [SerializeField] private Pivot _pivot;
+        [SerializeField] private PriorityAxis _priorityAxis;
         private Vector3 _center = Vector3.zero;
+
+        public void Rebuild(int itemsCount)
+        {
+            _positionsCount = Mathf.Clamp(itemsCount, 1, Int32.MaxValue);
+            Build();
+        }
 
         private void OnValidate()
         {
@@ -20,25 +27,46 @@ namespace Game.Scripts.UtilsSubmodule.Inventory
         private void Build()
         {
             SetCenter();
-            Grid = new Vector3[_positionsCount];
+            Positions = new Vector3[_positionsCount];
 
-            // int layersCount = (int)(_positionsCount / (_size.x * _size.y));
             int currentLayer = 0;
             int currentGridIndex = 0;
 
             while (currentGridIndex < _positionsCount)
             {
-                for (int x = 0; x < _size.x; x++)
+                switch (_priorityAxis)
                 {
-                    for (int z = 0; z < _size.y; z++)
-                    {
-                        Grid[currentGridIndex] = new(_center.x + x * _spacing.x,
-                            _center.y + _spacing.y * currentLayer,
-                            _center.z + z * _spacing.z);
-                        currentGridIndex++;
-                        if (currentGridIndex == _positionsCount) return;
-                    }
+                    case PriorityAxis.X:
+                        for (int x = 0; x < _size.x; x++)
+                        {
+                            for (int z = 0; z < _size.y; z++)
+                            {
+                                Positions[currentGridIndex] = new(_center.x + x * _spacing.x,
+                                    _center.y + _spacing.y * currentLayer,
+                                    _center.z + z * _spacing.z);
+                                currentGridIndex++;
+                                if (currentGridIndex == _positionsCount) return;
+                            }
+                        }
+
+                        break;
+
+                    case PriorityAxis.Z:
+                        for (int z = 0; z < _size.y; z++)
+                        {
+                            for (int x = 0; x < _size.x; x++)
+                            {
+                                Positions[currentGridIndex] = new(_center.x + x * _spacing.x,
+                                    _center.y + _spacing.y * currentLayer,
+                                    _center.z + z * _spacing.z);
+                                currentGridIndex++;
+                                if (currentGridIndex == _positionsCount) return;
+                            }
+                        }
+
+                        break;
                 }
+
 
                 currentLayer++;
             }
@@ -47,9 +75,9 @@ namespace Game.Scripts.UtilsSubmodule.Inventory
         private void OnDrawGizmos()
         {
             Gizmos.color = Color.red;
-            for (int i = 0; i < Grid.Length; i++)
+            for (int i = 0; i < Positions.Length; i++)
             {
-                Gizmos.DrawSphere(transform.position + Grid[i], .2f);
+                Gizmos.DrawSphere(transform.position + Positions[i], .2f);
             }
         }
 
@@ -82,6 +110,12 @@ namespace Game.Scripts.UtilsSubmodule.Inventory
             MiddleX,
             MiddleZ,
             Middle
+        }
+
+        private enum PriorityAxis
+        {
+            X,
+            Z
         }
     }
 }
