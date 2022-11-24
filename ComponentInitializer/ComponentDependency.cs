917e219a8356bd05d7e68b667dd36aab97faf0bf
tka2704 <tka2704@gmail.com>
2022-11-24 18:59:52 +0300

    component_dependency_attribute

---
+++ ComponentInitializer/ComponentDependency.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+
+namespace UtilsSubmodule.ComponentInitializer
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class ComponentDependency : Attribute
+    {
+        private readonly Type[] _types;
+
+        public ComponentDependency(params Type[] types)
+        {
+            _types = types;
+        }
+
+        public bool ComponentsCheck(Component component)
+        {
+            foreach (var type in _types)
+            {
+                if (component.TryGetComponent(type, out _)) continue;
+
+                Debug.LogWarning($"{component} requires {type} on GameObject");
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
--- ComponentInitializer/Editor/ComponentInitializer.cs
+++ ComponentInitializer/Editor/ComponentInitializer.cs
@@ -1,27 +1,48 @@
+using System.Reflection;
+using System.Threading.Tasks;
 using UnityEditor;
+using UnityEngine;
 
-[InitializeOnLoad]
-public class ComponentInitializer
+namespace UtilsSubmodule.ComponentInitializer.Editor
 {
-    static ComponentInitializer()
+    [InitializeOnLoad]
+    public class ComponentInitializer
     {
-        ObjectFactory.componentWasAdded += ComponentsAutoInit;
-
-        EditorApplication.quitting -= OnEditorQuiting;
-        EditorApplication.quitting += OnEditorQuiting;
-    }
-
-    private static void ComponentsAutoInit(UnityEngine.Component obj)
-    {
-        if (obj is IAutoInit autoinit)
+        static ComponentInitializer()
         {
-            autoinit.AutoInit();
-        }
-    }
+            ObjectFactory.componentWasAdded += ComponentsAutoInit;
+            ObjectFactory.componentWasAdded += AttributesCheck;
 
-    private static void OnEditorQuiting()
-    {
-        ObjectFactory.componentWasAdded -= ComponentsAutoInit;
-        EditorApplication.quitting -= OnEditorQuiting;
+            EditorApplication.quitting -= OnEditorQuiting;
+            EditorApplication.quitting += OnEditorQuiting;
+        }
+
+        private static void ComponentsAutoInit(Component obj)
+        {
+            if (obj is IAutoInit autoinit)
+            {
+                autoinit.AutoInit();
+            }
+        }
+
+        private static async void AttributesCheck(Component obj)
+        {
+            var attributes = obj.GetType().GetTypeInfo().GetCustomAttributes();
+            foreach (var attribute in attributes)
+            {
+                if (attribute is not ComponentDependency dependency) continue;
+                if (dependency.ComponentsCheck(obj)) continue;
+
+                await Task.Yield();
+                Object.DestroyImmediate(obj);
+            }
+        }
+
+        private static void OnEditorQuiting()
+        {
+            ObjectFactory.componentWasAdded -= ComponentsAutoInit;
+            ObjectFactory.componentWasAdded -= AttributesCheck;
+            EditorApplication.quitting -= OnEditorQuiting;
+        }
     }
 }
