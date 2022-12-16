e12ee894e608b76c1d98b273c3f57d1797f94779
tka2704 <tka2704@gmail.com>
2022-12-16 12:58:35 +0300

    serializable hash set

---
+++ SerializableHashSet.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace UtilsSubmodule
+{
+    [System.Serializable]
+    public class SerializableHashSet<T> : ISerializationCallbackReceiver
+    {
+        private HashSet<T> _hashSet = new ();
+
+        [SerializeField] private List<T> _serializableItems;
+
+        void ISerializationCallbackReceiver.OnBeforeSerialize()
+        {
+           _serializableItems = new List<T>(_hashSet);
+            _hashSet = null;
+        }
+
+        void ISerializationCallbackReceiver.OnAfterDeserialize()
+        {
+            foreach (T item in _serializableItems)
+            {
+                _ = _hashSet.Add(item);
+            }
+
+            _serializableItems = null;
+        }
+
+        public bool Add(T item) => _hashSet.Add(item);
+
+        public bool Contains(T item) => _hashSet.Contains(item);
+    }
+}
