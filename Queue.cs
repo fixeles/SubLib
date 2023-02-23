37a91642f86ee62bd8bd420537a3ba4e3cb43ef4
tka2704 <tka2704@gmail.com>
2023-02-23 23:35:19 +0300

    queue

---
+++ Queue.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections.Generic;
+
+namespace SubLib
+{
+    public class Queue<T>
+    {
+        public event Action OnQueueChangeEvent;
+        private readonly int _maxCapacity;
+        private readonly List<T> _items;
+
+        public T[] Items => _items.ToArray();
+        public bool HasSpace => _items.Count < _maxCapacity;
+
+        public Queue(int maxCapacity)
+        {
+            _maxCapacity = maxCapacity;
+            _items = new List<T>(_maxCapacity);
+        }
+
+
+        public bool Enqueue(T item)
+        {
+            if (!HasSpace) return false;
+            _items.Add(item);
+            OnQueueChangeEvent?.Invoke();
+            return true;
+        }
+
+        public T Dequeue()
+        {
+            if (_items.Count == 0)
+            {
+                throw new InvalidOperationException("Queue is empty");
+            }
+
+            T item = _items[0];
+            _items.RemoveAt(0);
+            OnQueueChangeEvent?.Invoke();
+            return item;
+        }
+
+        public T Peek()
+        {
+            if (_items.Count == 0)
+            {
+                throw new InvalidOperationException("Queue is empty");
+            }
+
+            return _items[0];
+        }
+
+        public void Clear()
+        {
+            _items.Clear();
+        }
+    }
+}
